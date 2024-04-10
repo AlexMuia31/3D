306f1dae9061e9d4cd274382fc17df7a16db6258
AlexMuia31 <alexmuia31@gmail.com>
2024-04-10 21:22:49 +0300

    added video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = React.useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  });
+
   useGSAP(() => {
     gsap.to("#hero", { opacity: 1, delay: 1.5 });
   }, []);
@@ -12,6 +32,17 @@ const Hero = () => {
         <p id="hero" className="hero-title">
           iphone 15 pro
         </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
